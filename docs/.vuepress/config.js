8cd6affcac505118da660c9909d91d3622db5029
biyuqi <biyuqiwan@163.com>
2020-06-29 00:19:40 +0800

    chore: 边栏基础设施配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,25 @@
+const devops = require('./devops')
+
 module.exports = {
-  title: 'LoadingMore\'s Notes',
+  title: 'LoadingMore\'s blog',
   description: '前端成长之路',
+  base: '/',
   themeConfig: {
     nav: [
       { text: '主页', link: '/' },
-      { text: '博客', link: '/blog' },
-      { text: '前端工程化', link: '/frontend-engineering' },
-      { text: 'Node实战', link: '/node-practice' },
-      { text: 'Devops实践', link: '/devops-practice' },
-      { text: '可视化实践', link: '/visualization-practice' },
+      { text: '博客', link: '/blog/' },
+      { text: '前端工程化', link: '/frontend-engineering/' },
+      { text: 'Node实战', link: '/node-practice/' },
+      { text: 'Devops实践', link: '/devops-practice/' },
+      { text: '可视化实践', link: '/visualization-practice/' },
       { text: 'Github', link: 'https://github.com/BiYuqi/memory-leak' },
-    ]
+    ],
+    sidebar: {
+      '/blog/': [],
+      '/frontend-engineering/': [],
+      '/node-practice/': [],
+      '/devops-practice/': devops,
+      '/visualization-practice/': []
+    }
   }
 }
---
+++ docs/.vuepress/devops.js
@@ -0,0 +1,9 @@
+module.exports = [
+  {
+    title: '初始化',
+    collapsable: false,
+    children: [
+      ['', '服务器登录配置']
+    ]
+  }
+]
